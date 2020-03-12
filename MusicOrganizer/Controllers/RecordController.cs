dee451a92457ddcf274307d4530fbeb41a5e985f
Katy Solovewicz <katy@solovewicz.com>
2020-03-12 11:43:55 -0700

    add routes in RecordController

--- MusicOrganizer/Controllers/RecordController.cs
+++ MusicOrganizer/Controllers/RecordController.cs
@@ -6,11 +6,31 @@ namespace MusicOrganizer.Controllers
   public class RecordController : Controller
   {
 
-    [HttpGet("/")]
+    [HttpGet("/records")]
     public ActionResult Index()
+    {
+      List<Record> allRecords = Record.GetAll();
+      return View(allRecords);
+    }
+
+    [HttpGet("/records/new")]
+    public ActionResult New()
     {
       return View();
     }
 
+    [HttpPost("/records")]
+    public ActionResult Create(string title, string artist)
+    {
+      Record newRecord = new Record(title, artist);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/records/{id}")]
+    public ActionResult Show(int id)
+    {
+      Record selectedRecord = Record.Find(id);
+      return View(selectedRecord);
+    }
   }
 }
